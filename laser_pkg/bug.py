db6452742254b1a77a53be16bfa9c0214c004651
Leonardo Cardenas <leonardo.cardenas.martine02@gmail.com>
2024-03-27 00:26:02 -0600

    bug2 mejorado, continua error de reverseo

--- laser_pkg/bug.py
+++ laser_pkg/bug.py
@@ -49,6 +49,10 @@ class MoveRobotNode(Node):
         self.colision_pose.x = 0.0
         self.colision_pose.y = 0.0
         
+        self.salida_pose = Pose2D()
+        self.salida_pose.x = 0.0
+        self.salida_pose.y = 0.0
+
         self.start_path = False
 
         self.obstacle =  dict(obstacle=False,orientation=None)
@@ -59,13 +63,14 @@ class MoveRobotNode(Node):
         self.prev_ = 0
 
         self.first_time = True
+        self.first_time_out = True
         
         self.colision = False
         self.flag_colision = False
-        self.flag_colision2 = False
+        self.flag_salida = False
         self.rodeado = False
-        self.done = False
         self.redzone = False
+        self.greenzone = False
     #FUNCION LLAMADA CADA QUE SE RECIBE MSG EN LASER 
     
     def laserscan_callback(self,msg):
@@ -76,7 +81,23 @@ class MoveRobotNode(Node):
         self.obstacle_right = any(r < self.umbral for r in msg.ranges[90:179])
         self.obstacle_left = any(r < self.umbral for r in msg.ranges[181:270])
         
+    # FUNCION LLAMADA CADA QUE SE RECIBE MSG EN ODOM
+    def odom_callback(self, msg_odom):
+        
+        self.pose.x = round(msg_odom.pose.pose.position.x,4)
+        self.pose.y = round(msg_odom.pose.pose.position.y,4)
+        orientation = msg_odom.pose.pose.orientation
+        
+        (xq,yq,zq,wq) = (orientation.x, orientation.y, orientation.z, orientation.w)
+        t3 = +2.0 * (wq*zq+xq*yq)
+        t4 = +1.0 - 2.0 * (yq*yq+zq*zq)
+        self.theta = math.atan2(t3,t4)
+        
+    # ERROR AL GOAL
+    def euclidean_distance(self, goal_pose):
+        return sqrt(pow((goal_pose.x - self.pose.x),2)+pow((goal_pose.y-self.pose.y),2))
 
+    # BOUNDARY FOLLOW
     def boundary_follow(self):
         
         if not self.bandera:
@@ -130,21 +151,6 @@ class MoveRobotNode(Node):
             self.mover(self.v , self.w)
 
 
-    # FUNCION LLAMADA CADA QUE SE RECIBE MSG EN ODOM
-    def odom_callback(self, msg_odom):
-        
-        self.pose.x = round(msg_odom.pose.pose.position.x,4)
-        self.pose.y = round(msg_odom.pose.pose.position.y,4)
-        orientation = msg_odom.pose.pose.orientation
-        
-        (xq,yq,zq,wq) = (orientation.x, orientation.y, orientation.z, orientation.w)
-        t3 = +2.0 * (wq*zq+xq*yq)
-        t4 = +1.0 - 2.0 * (yq*yq+zq*zq)
-        self.theta = math.atan2(t3,t4)
-        
-    # error hacia el objetivo
-    def euclidean_distance(self, goal_pose):
-        return sqrt(pow((goal_pose.x - self.pose.x),2)+pow((goal_pose.y-self.pose.y),2))
 
     # FUNCION PARA CALCULAR LEY DE CONTROL
     def ley_control(self,x,y,theta):
@@ -152,16 +158,13 @@ class MoveRobotNode(Node):
         # Error a
         x_err = self.goal_pose.x - x
         y_err = self.goal_pose.y - y
-    
         a = sqrt(x_err**2 + y_err**2)
 
         if(theta > math.pi):
             theta = theta - 2 * math.pi
-
         if(theta < -math.pi):
             theta = theta + 2 * math.pi
             
-        
         # Error alpha
         alpha = atan2(y_err,x_err) - theta 
         
@@ -170,7 +173,6 @@ class MoveRobotNode(Node):
 
         if v > 1:
             v = 1.0
-
         if v < -1:
             v = -1.0
         
@@ -178,17 +180,12 @@ class MoveRobotNode(Node):
         
         if w > 1.0:
             w = 1.0
-
         if w < -1.0:
             w = -1.0
 
-        #publica cada 1seg
-        #self.get_logger().info(f'goal pose: ({self.goal_pose.x} , {self.goal_pose.y}) - robo pose: ({round(x,2)},{round(y,2)}) - theta: {round(theta,2)} - {round(theta*180/math.pi,2)}', throttle_duration_sec=0.5)
-
-        #print('goal pose: (',self.goal_pose.x ,' , ', self.goal_pose.y,')   robo pose: (',round(x,2),',', round(y,2),' ) ',' , ',theta,theta*180/math.pi,')')
-
         self.mover(v,w)        
 
+    # MOVE TO GOAL
     def move2goal(self):
 
         #mover hacia el objetivo
@@ -207,9 +204,12 @@ class MoveRobotNode(Node):
             self.goal_pose.y = float(point_y)
             self.colision_pose.x = 0.0
             self.colision_pose.y = 0.0
+            self.salida_pose.x = 0.0
+            self.salida_pose.y = 0.0
             self.flag_colision = False
+            self.flag_salida = False
             self.first_time = True
-
+            self.first_time_out = True
             self.start_path = True
         
         distance_tolerance = 0.15
@@ -234,6 +234,7 @@ class MoveRobotNode(Node):
             else:
                 sys.exit()
 
+    # FUNCION CRITERIO PARA SALIR DE BOUNDARY FOLLOW
     def alineado(self):
         x1, y1 = self.colision_pose.x, self.colision_pose.y  # Coordenadas del punto A
         x2, y2 = self.pose.x, self.pose.y  # Coordenadas del punto B
@@ -247,38 +248,7 @@ class MoveRobotNode(Node):
             self.rodeado = False
             print('no alineado')
 
-    def cambiodesigno(self):
-        #--------------------------------------------------------------------
-        # Calculo cambio de signo en calculos con vectores
-        #--------------------------------------------------------------------
-        n = []
-     	#Calcular Vector XoXf
-        vx = self.goal_pose.x - self.colision_pose.x
-        vy = self.goal_pose.y - self.colision_pose.y
-        
-     	#rotar
-        n.append(-vy)
-        n.append(vx)
-        
-        #ODOM - X0
-        ODO_X0 = self.pose.x - self.colision_pose.x
-        ODO_Y0 = self.pose.y - self.colision_pose.y
-        
-        #MULTIPLICAR
-        calculos_signo = (ODO_X0*n[0]) + (ODO_Y0*n[1])
-        
-        #print(str(calculos_signo))
-        #no deberia cambiar
-        #print(str(self.colision_pose.x)+','+str(self.colision_pose.y))
-	
-        if((calculos_signo >= 0 and self.prev_ < 0) or (calculos_signo < 0 and self.prev_ >= 0)):
-            self.first_time = True
-            self.get_logger().info("CAMBIO SIGNO")
-        
-        self.prev_ = calculos_signo
-        
-        #---------------------------------------------------------------------
-
+    # FUNCION PARA EVITAR CONFLICTOS ENTRE CAMBIO DE COMPORTAMIENTOS FINAL
     def transicion(self):
         self.stop_robot()
         rango = self.rango
@@ -300,9 +270,8 @@ class MoveRobotNode(Node):
         if erra <= 0.03: self.mover(0.4 , 0.0)
         else: self.mover(0.0 , erra )
 
-
-    def prueba (self):
-        self.alineado()
+    # FUNCION PARA GENERAR ZONA DE COLISION Y EVITAR CONFLICTOS EN CAMBIO DE COMPORTAMIENTO INICIAL
+    def red_zone(self):
         if self.first_time and self.obstacle2:
             self.colision_pose.x = self.pose.x
             self.colision_pose.y = self.pose.y
@@ -314,23 +283,40 @@ class MoveRobotNode(Node):
 
         print('colision:',self.colision_pose,'separacion:',separacion,'redzone:',self.redzone)
     
-        if self.obstacle2 : print('colision')
-        else: print('no colision')
+    # FUNCION PARA GENERAR ZONA DE SALIDA Y EVITAR CONFLICTOS EN CAMBIO DE COMPORTAMIENTO FINAL
+    def green_zone(self):
+        if self.first_time_out and self.rodeado and not self.redzone:
+            self.salida_pose.x = self.pose.x
+            self.salida_pose.y = self.pose.y
+            self.first_time_out = False
+        #region de salida (distancia entre pose y salida)
+        separacion =  math.sqrt((self.pose.x - self.salida_pose.x)**2 + (self.pose.y - self.salida_pose.y)**2 ) 
+        if separacion < 0.5: self.greenzone = True
+        if separacion > 0.9: self.greenzone = False
 
-        if self.obstacle3 : print('No libre')
-        else: print('libre')    
+        print('salida:',self.salida_pose,'separacion:',separacion,'redgreenzone:',self.greenzone)
+
+    # FUNCION EN BUCLE
+    def prueba (self):
+        self.alineado() #Caclula si se encuentra alineado
+        self.red_zone() 
+        self.green_zone()  
             
         if not self.flag_colision and self.obstacle2: 
             self.flag_colision = True
         if self.flag_colision: 
-            print('flag')
+            print('red flag')
 
+        if not self.flag_salida and self.rodeado: 
+            self.flag_salida = True
+        if self.flag_salida: 
+            print('green flag')
         
         if self.flag_colision:
-            if self.redzone or not self.rodeado : self.boundary_follow()
+            if self.redzone or not self.rodeado and not self.greenzone : self.boundary_follow()
             if not self.redzone:
-                if self.rodeado: 
-                    self.transicion()
+                if self.rodeado and self.green_zone: 
+                    self.move2goal()
                     if not self.obstacle3 : self.flag_colision = False
         else: self.move2goal()
 
